4322be4c5a034cefa7ae981a959461b6efc496c2
IT24102381 <it24102381@my.sliit.lk>
2025-09-20 07:46:49 +0530

    initial commit

---
+++ src/main/java/com/sliit/event_photography_management_system/EventPhotographyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.event_photography_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPhotographyManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventPhotographyManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sliit/event_photography_management_system/EventPhotographyManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.event_photography_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPhotographyManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
